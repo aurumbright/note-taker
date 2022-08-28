770adf5487f98af80a4a93be330bc6e9bbf42958
aurumbright <aurum.elean@gmail.com>
2022-08-28 08:42:10 -0700

    delete path added

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/helper');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/helper');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
@@ -25,6 +26,29 @@ notes.post('/', (req, res) => {
     } else {
         res.error('Error in adding note');
     }
+});
+
+// DELETE Route for a new note
+notes.delete('/:id', (req, res) => {
+
+    let noteList = require('../db/db.json');
+
+    for (let i = 0; i < noteList.length; i++) {
+
+        if (req.params.id === noteList[i].id) {
+            noteList.splice(i, 1);
+        }
+    }
+
+    fs.writeFile("./db/db.json", JSON.stringify(noteList), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!");
+        }
+    });
+    res.json(noteList);
 });
 
 module.exports = notes;
